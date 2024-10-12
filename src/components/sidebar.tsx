95f82106d69bbebcf3f6004dfdbabf8045f97c2a
ITKmz <ittikorn.mutu@kmutt.ac.th>
2024-10-12 17:46:38 +0700

    feat: sidebar

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,14 @@
+import { siteConfig } from "@/config/site";
+import { Logo } from "@/components/icons";
+import { Link } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
+
+// Export sidebar component
+
+export const Sidebar = () => {
+  return (
+    <aside className="fixed h-full border-r w-64">
+        
+    </aside>
+  );
+};
