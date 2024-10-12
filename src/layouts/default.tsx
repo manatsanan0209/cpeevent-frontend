af667fde79e05b072cb30240559642062e3c9a56
ITKmz <ittikorn.mutu@kmutt.ac.th>
2024-10-12 22:39:20 +0700

    fix: layouts

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -10,23 +10,25 @@ export default function DefaultLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative flex flex-col h-screen">
-      <Navbar />
+    <div className="relative flex h-screen">
       <Sidebar />
-      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
-        {children}
-      </main>
-      <footer className="w-full flex items-center justify-center py-3">
-        <Link
-          isExternal
-          className="flex items-center gap-1 text-current"
-          href="https://nextui-docs-v2.vercel.app?utm_source=next-pages-template"
-          title="nextui.org homepage"
-        >
-          <span className="text-default-600">Powered by</span>
-          <p className="text-primary">NextUI</p>
-        </Link>
-      </footer>
+      <div className="flex flex-col w-full h-full">
+        <Navbar />
+        <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
+          {children}
+        </main>
+        <footer className="w-full flex  items-center justify-center py-3">
+          <Link
+            isExternal
+            className="flex items-center gap-1 text-current"
+            href="https://nextui-docs-v2.vercel.app?utm_source=next-pages-template"
+            title="nextui.org homepage"
+          >
+            <span className="text-default-600">Powered by</span>
+            <p className="text-primary">NextUI</p>
+          </Link>
+        </footer>
+      </div>
     </div>
   );
 }
