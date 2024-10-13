4e67be175249f8f7883a0b2ac33c18261fa1bde3
Encall <apichataimimpat@gmail.com>
2024-10-13 17:55:46 +0700

    feat: implement axios configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-import App from "./App.tsx";
-import { Provider } from "./provider.tsx";
-import "@/styles/globals.css";
+import App from './App.tsx';
+import { Provider } from './provider.tsx';
+import '@/styles/globals.css';
+import './api/axios-config';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <>
-    <BrowserRouter>
-      <Provider>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+   <>
+      <BrowserRouter>
+         <Provider>
+            <App />
+         </Provider>
+      </BrowserRouter>
+   </>,
 );
