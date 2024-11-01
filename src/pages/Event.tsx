ed4e7823c185de2e3fc9127804144851494329a7
encall <apichataimimpat@gmail.com>
2024-11-01 18:08:39 +0700

    feat: implement event fetching from API

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,13 +1,32 @@
 import { Tabs, Tab } from '@nextui-org/react';
+import { useEffect, useState } from 'react';
 
 import { events } from '../data/events.ts';
 import { user } from '../data/user.ts';
 
+import { axiosAPIInstance } from '@/api/axios-config.ts';
 import DefaultLayout from '@/layouts/default';
 import AllEvent from '@/components/eventComponents/AllEvent.tsx';
 import JoinedEvent from '@/components/eventComponents/JoinedEvent.tsx';
 
 export default function Event() {
+    const [allEventData, setAllEventData] = useState([]);
+
+    useEffect(() => {
+        const fetchEvents = async () => {
+            try {
+                const response = await axiosAPIInstance.get('v1/events');
+
+                setAllEventData(response.data);
+            } catch (error) {
+                console.error('Error fetching events:', error);
+            }
+        };
+
+        fetchEvents();
+        console.log(allEventData);
+    }, []);
+
     return (
         <DefaultLayout>
             <Tabs
