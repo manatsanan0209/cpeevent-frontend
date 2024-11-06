59f8df3000460be54de2f7e47e1382247be1a508
Wongsatorn Sungsilpawech <wongsatorn.sung@hotmail.com>
2024-11-07 01:10:22 +0700

    Added users info value in account page

--- src/components/settings/account.tsx
+++ src/components/settings/account.tsx
@@ -1,18 +1,78 @@
+import type { UserAccountType } from '@/types/index';
+
 import { Divider, Input, Button } from '@nextui-org/react';
+import { useState, useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+import { axiosAPIInstance } from '@/api/axios-config.ts';
 
 interface InputFieldProps {
     label: string;
     placeholder: string;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const InputField = ({ label, placeholder }: InputFieldProps) => (
+const InputField = ({
+    label,
+    placeholder,
+    value,
+    onChange,
+    name,
+}: InputFieldProps & { name: string }) => (
     <div className="flex flex-col gap-2">
         <p className="text-zinc-500">{label}</p>
-        <Input placeholder={placeholder} />
+        <Input
+            name={name}
+            placeholder={placeholder}
+            type="text"
+            value={value}
+            onChange={onChange}
+        />
     </div>
 );
 
 export default function Account() {
+    // const { user } = useContext(AuthContext);
+    const [userInfo, setUserInfo] = useState<UserAccountType>({
+        studentID: '',
+        firstName: '',
+        lastName: '',
+        year: 1,
+        imgProfile: '',
+        email: '',
+        password: '',
+        phoneNumber: '',
+        username: '',
+        created_at: new Date(),
+    });
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+
+        setUserInfo((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    const fetchProfile = async () => {
+        const response = await axiosAPIInstance.get('v1/account/info');
+
+        return response.data.data;
+    };
+
+    const { data: userData } = useQuery<UserAccountType>({
+        queryKey: ['studentid'],
+        queryFn: fetchProfile,
+    });
+
+    useEffect(() => {
+        if (userData) {
+            setUserInfo(userData);
+        }
+    }, [userData]);
+
     return (
         <div className="flex flex-col">
             <h3 className="text-2xl font-bold text-zinc-600">Account</h3>
@@ -29,6 +89,7 @@ export default function Account() {
                         isReadOnly
                         description="You can't change your student ID."
                         placeholder="Student ID"
+                        value={userInfo.studentID}
                     />
                 </div>
             </div>
@@ -39,22 +100,49 @@ export default function Account() {
                 </h4>
                 <div className="w-full flex flex-row gap-2 gap-x-4 mt-4">
                     <div className="w-1/2">
-                        <InputField label="Name" placeholder="Name" />
+                        <InputField
+                            label="Name"
+                            name="firstName"
+                            placeholder="Name"
+                            value={userInfo.firstName}
+                            onChange={handleInputChange}
+                        />
                     </div>
                     <div className="w-1/2">
-                        <InputField label="Surname" placeholder="Surname" />
+                        <InputField
+                            label="Surname"
+                            name="lastName"
+                            placeholder="Surname"
+                            value={userInfo.lastName}
+                            onChange={handleInputChange}
+                        />
                     </div>
                 </div>
                 <div className="mt-4">
-                    <InputField label="Year" placeholder="Year" />
+                    <InputField
+                        label="Year"
+                        name="year"
+                        placeholder="Year"
+                        value={userInfo.year.toString()}
+                        onChange={handleInputChange}
+                    />
                 </div>
                 <div className="mt-4">
-                    <InputField label="Email" placeholder="Email" />
+                    <InputField
+                        label="Email"
+                        name="email"
+                        placeholder="Email"
+                        value={userInfo.email}
+                        onChange={handleInputChange}
+                    />
                 </div>
                 <div className="mt-4">
                     <InputField
                         label="Phone Number"
+                        name="phoneNumber"
                         placeholder="Phone Number"
+                        value={userInfo.phoneNumber}
+                        onChange={handleInputChange}
                     />
                 </div>
             </div>
